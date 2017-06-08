095b4bd756efadf60265a8c06ab2c4a48e2261cb
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2017-06-07 22:20:22 -0700

    added using in module file

--- src/BrindlePlots.jl
+++ src/BrindlePlots.jl
@@ -3,9 +3,13 @@
 module BrindlePlots
 
 using Gadfly
+using BufferedStreams
+using Libz
+using DataFrames
 
 include("themes.jl")
 include("elements.jl")
+include("input.jl")
 
 export BrindlePlot
 
