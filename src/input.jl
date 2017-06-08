7a348c63e44c3289a2c1237f66dbf49270112095
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2017-06-07 17:00:53 -0700

    updated input to add open streams code

--- src/input.jl
+++ src/input.jl
@@ -4,6 +4,27 @@
 =#
 
 parse_complexity{S <: AbstractString}( c::S ) = split( c, COMPLEX_CHAR, keep=false )[1] |> x->parse(Int,x)
+parse_float_omit_text{S <: AbstractString}( str::S, header::String ) = str != "NA" && str != header ? parse(Float64, str) : 0.0
+
+
+function open_stream( filename )
+   fopen = open( filename, "r" )
+   if isgzipped( filename )
+      stream = ZlibInflateInputStream( fopen, reset_on_end=true )
+   else
+      stream = BufferedStreams.BufferedInputStream( fopen )
+   end
+   stream
+end
+
+function open_streams( files::Vector{String} )
+   buf = Vector{BufferedStreams.BufferedInputStream}(length(files))
+   for i in 1:length(files)
+      buf[i] = open_stream( files[i] )
+   end
+   buf
+end
+
 
 # OR we serialize whippet data for BrindlePlots.jl?
 type WhippetNode
