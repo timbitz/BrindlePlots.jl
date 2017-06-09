c2081b27086d2a036f6584bcc733b7434779f5f6
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2017-06-09 11:36:30 -0700

    overhaul of types

--- src/types.jl
+++ src/types.jl
@@ -26,6 +26,18 @@ type BrindleEdgeSet
    maxvalue::Float64
 end
 
+type BrindleNodeSet
+   map::Dict{Int,BrindleNode}
+   range::UnitRange
+end
+
+immutable BrindleEvent
+   edgeset::BrindleEdgeSet
+   nodeset::BrindleNodeSet
+   chr::String
+   strand::Bool
+end
+
 type BrindlePath
    path::IntSet
    length::Int
@@ -38,7 +50,7 @@ function Base.parse(::Type{BrindleNode}, str::String, psi::Float64)
    BrindleNode( string(colon[1]), parse(Int, string(ints[1])), parse(Int, string(ints[2])), psi )
 end
 
-function Base.parse{S <: AbstractString}(::Type{BrindleEdge}, str::S )
+function Base.parse(::Type{BrindleEdge}, str::String )
    colon = split( string(str), ':' )
    ints  = split( string(colon[1]), '-' )
    first = parse(Int, string(ints[1]))
@@ -60,5 +72,27 @@ function Base.parse(::Type{BrindleEdgeSet}, str::String )
       maxval = be.value > maxval ? be.value : maxval
    end
    BrindleEdgeSet( edges, nodes, maxval )
+end
+
+function BrindleEvent( genedf::DataFrame, node::Int )
+   edgeset = parse(BrindleEdgeSet, genedf[(genedf[:,:Node] .== node),:Edges][1])
+   nodes = Dict{Int,BrindleNode}()
+   lower,upper = Inf,-Inf
+   chr,strand  = "",true
+
+   # draw exons
+   for n in edgeset.nodes
+      (length(genedf[(genedf[:,:Node] .== n),:Coord]) == 0) && continue
+      psi = genedf[(genedf[:,:Node] .== n),:Psi][1]
+      strand = genedf[(genedf[:,:Node] .== n),:Strand][1] == "+" ? true : false
+      cnode = parse(BrindleNode, genedf[(genedf[:,:Node] .== n),:Coord][1], isna(psi) ? 1.0 : psi)
+      chr = cnode.chr
+      nodes[n] = cnode
+      lower = lower > cnode.first ? cnode.first : lower
+      upper = upper < cnode.last  ? cnode.last  : upper
+   end
+   nodeset = BrindleNodeSet( nodes, lower:upper )
+
+   BrindleEvent( edgeset, nodeset, chr, strand )
 end
 
