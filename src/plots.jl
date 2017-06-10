f04ff72dfed3537ad7f781cf04563628babe832b
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2017-06-10 10:30:57 -0700

    improve plot resizing based on sample number

--- src/plots.jl
+++ src/plots.jl
@@ -10,18 +10,18 @@ function make_plots( delta::BufIn, tables::Vector{DataFrame}, samples::Vector{St
       node    = parse(Int, nodestr)
       eventlayers,chr,xmin,xmax = draw_events( tables, samples, geneid, node )
       labelspace = (xmax-xmin)*0.3 + xmax
-      set_plot_size(length(tables))
+      #set_plot_size(length(tables))
       eventplot = plot(eventlayers, Guide.xlabel(chr), Guide.ylabel(""), Guide.yticks(ticks=nothing), 
                        default_theme(), Coord.cartesian(xmin=xmin, xmax=labelspace, ymin=0, ymax=length(samples)+1),
                        Guide.title("Local Splicing Event (LSE) Graphs"))
       gellayers,agarose = draw_insilico_gel( tables, samples, geneid, node )
-      gelplot   = plot(gellayers, Coord.cartesian(ymin=DEFAULT_MAXDIST*-1 - 3, ymax=5, xmin=-0.75, xmax=length(samples)+0.5), 
+      gelplot   = plot(gellayers, Coord.cartesian(ymin=DEFAULT_MAXDIST*-1 - 3, ymax=5, xmin=-1, xmax=length(samples)+0.5), 
                        default_theme(), Guide.xticks(ticks=nothing), Guide.yticks(ticks=nothing),
                        Guide.xlabel(""), Guide.ylabel(""), Guide.colorkey("PSI"),
                        Guide.title("$(round(agarose,2))% Agarose Gel"))
-      gelgrid   = length(samples) > 2 ? vstack(gelplot, Compose.context()) : gelplot
-      draw( PDF("$geneid\_$nodestr\_$(basename(filename)).pdf", plot_dimensions( length(tables) )...), 
-            hstack(eventplot, gelgrid) )
+      grid      = hstack(compose(context(0, 0, eventplot_dimensions(length(tables))...), render(eventplot)),
+                         compose(context(0, 0, gelplot_dimensions(length(tables))...), render(gelplot))) 
+      draw( PDF("$geneid\_$nodestr\_$(basename(filename)).pdf", plot_dimensions( length(tables) )...), grid )
    end
    true
 end
