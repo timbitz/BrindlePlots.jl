92c5ea90f27cb2a33b6ab7b9bd3de94046a7087a
Garrett Massman <gmassman20@gmail.com>
2019-11-17 20:37:08 -0800

    updated travis yaml

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using BufferedStreams
 
 @testset "Basic Single Plot" begin
 
-   delta = BufferedInputStream(IOBuffer("Gene\tNode\tBlank\nENSG00000117448.13_2\t10\tblank\nENSG00000152078.9_2\t3\tblank\n"))
+   delta = BufferedInputStream(IOBuffer("Gene\tNode\tBlank\nENSG00000117448.13_2\t10\tblank\n"))
    samples = ["test.psi.gz", "test2.psi.gz"]
    tables = load_tables( map(x->"$(dir)/$(x)", samples) )
    @test make_plots( delta, tables, samples, "testplot" ) == true
